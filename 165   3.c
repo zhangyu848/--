30197d334397c46cffe14aed726859c163de82e8
盖世英雄 <3596798997@qq.com>
2025-11-25 21:59:02 +0800

    二维数组

---
+++ 165   3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int a[3][3];
+    int i,j;
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+    int sum=0;
+    for(i=0;i<3;i++){
+        sum+=a[i][i];
+    }
+    printf("%d",sum);
+    return 0;
+}
