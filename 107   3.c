169e370ed65c55ca8fad6a28e1f46dee8520f0ee
盖世英雄 <3596798997@qq.com>
2025-11-25 17:31:47 +0800

    ok

---
+++ 107   3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int max(int a,int b,int c)
+{
+    if(a>=b && a>=c)
+        return a;
+    else if(b>=a && b>=c)
+        return b;
+    else
+        return c;
+}
+int main()
+{
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    printf("%d\n", max(a,b,c));
+    return 0;
+}   
